5147f67801665374a998e26e1227dcee3439ed08
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-09-14 12:07:17 +0300

    Create Sorting.cpp

---
+++ Sorting.cpp
@@ -0,0 +1,104 @@
+﻿﻿#include <iostream>
+#include <chrono>
+#include <future>
+
+#define SIZE 2000000
+
+bool make_threads = false;
+
+void merge(int* arr, int l, int m, int r)
+{
+    int nl = m - l + 1;
+    int nr = r - m;
+      
+    int* left = new int[nl];
+    int* right = new int[nr];
+
+   
+    for (int i = 0; i < nl; i++)
+        left[i] = arr[l + i];
+
+    for (int j = 0; j < nr; j++)
+        right[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0;
+    int k = l;  
+
+    while (i < nl && j < nr) {
+        
+        if (left[i] <= right[j]) {
+            arr[k] = left[i];
+            i++;
+        }
+        else {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+   
+    while (i < nl) {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+   
+    while (j < nr) {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+
+    delete[] left;
+    delete[] right;
+}
+
+void mergeSort(int* arr, int l, int r)
+{
+    if (l >= r)
+    {
+        return;
+    }
+
+    int m = (l + r - 1) / 2;
+    if (make_threads && (m - l) > 10000)
+    {
+        std::future<void> f = std::async(std::launch::async, [&]() {mergeSort(arr, l, m); });
+        mergeSort(arr, m + 1, r);
+
+        try
+        {
+            f.get();
+        }
+        catch (...)
+        {
+            std::cout << "an exception is occured..." << std::endl;
+        }
+    }
+    else
+    {
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+    }
+    merge(arr, l, m, r);
+}
+
+int main()
+{
+    int* rand_arr = new int[SIZE];
+    std::srand(time(nullptr));
+
+    for (long i = 0; i < SIZE; i++)
+    {
+        rand_arr[i] = rand() % 500;
+    }
+    
+    make_threads = true;
+    std::cout << "multiply threads sorting for array dimension N = " << SIZE << " elements" << std::endl;
+    mergeSort(rand_arr, 0, SIZE - 1); 
+    
+  
+
+
+
+   
